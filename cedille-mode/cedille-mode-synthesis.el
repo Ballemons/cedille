1c96baea0b1c51bf86cb675a07a8941ccb4f30fc
Pedro Abreu <abreu223@hotmail.com>
2019-09-02 18:19:46 -0400

    First steps to have a good name of variables

--- cedille-mode/cedille-mode-synthesis.el
+++ cedille-mode/cedille-mode-synthesis.el
@@ -1,6 +1,25 @@
 ;;; cedille-mode-synthesis.el --- description -*- lexical-binding: t; -*-
 
 ;;; Code:
+;;;
+
+(require 'subr-x)
+
+(defun alist-set (key val alist &optional symbol)
+  "Set property KEY to VAL in ALIST. Return new alist.
+This creates the association if it is missing, and otherwise sets
+the cdr of the first matching association in the list. It does
+not create duplicate associations. By default, key comparison is
+done with `equal'. However, if SYMBOL is non-nil, then `eq' is
+used instead.
+
+This method may mutate the original alist, but you still need to
+use the return value of this method instead of the original
+alist, to ensure correct results."
+  (if-let ((pair (if symbol (assq key alist) (assoc key alist))))
+      (setcdr pair val)
+    (push (cons key val) alist))
+  alist)
 
 (defun get-span-type(data)
   "Filter out special attributes from the data in a span"
@@ -9,19 +28,33 @@
 (defun get-span-name(data)
   (cdr (assoc 'name data)))
 
-(defun synth-foralls(type)
-  (setq type (replace-regexp-in-string "∀" "Λ" type))
-  (setq type (replace-regexp-in-string "Π" "λ" type))
-  (while (string-match "\\. \\([^\\.➔]*\\) ➔" type)
-    (setq s0 (downcase (match-string 1 type)))
-    (setq s (concatenate 'string ". λ " s0))
-    (setq s1 (concatenate 'string s " : \\1 ."))
-    (setq type (replace-match s1 nil nil type))
+(defun desambiguate-lambdas(term)
+  (setq vars nil)
+  (setq start 0)
+  (while (string-match "λ \\(.*\\) : .* \\." term start)
+    (setq start (match-end 0))
+    (setq var (match-string 1 term))
+    (if (assoc var vars)
+        (progn ;; if branch
+          (setq occs (assoc var vars))
+          (alist-set var (+1 occs) vars) ;; update the value of key=var
+          )
+      (cons '(var . 0) vars) ;; else branch
+      )
     )
-  (replace-regexp-in-string "\\.[^\\.]*$" ". " type)
-  ;; (setq type (replace-regexp-in-string "[\\.➔] \\([^\\.➔]*?\\) [➔]" ". λ ?\\1 ." type))
-  ;; (replace-regexp-in-string "\\..*?$" "asdf" type)
-  ;; (match-string )
+  )
+
+(defun synth-hole(type)
+  (setq type (replace-regexp-in-string "∀" "Λ" type)) ;; Replace foralls
+  (setq type (replace-regexp-in-string "Π" "λ" type)) ;; Replace Pis
+  (while (string-match "\\. \\([^\\.➔]*\\) ➔" type) ;; Create lambdas from arrows
+    (setq s (downcase (match-string 1 type)))
+    (setq s (concatenate 'string ". λ " s))
+    (setq s (concatenate 'string s " : \\1 ."))
+    (setq type (replace-match s nil nil type))
+    )
+  (replace-regexp-in-string "\\.[^\\.]*$" ". " type) ;; Delete the final return type
+  ;; (desambiguate-lambdas type)
   )
 
 (defun cedille-mode-synth-quantifiers ()
@@ -33,7 +66,7 @@ the quantifiers at the given hole"
            (d (se-term-to-json term))
            (name (se-term-name term))
            (type (get-span-type d))
-           (synth-type (synth-foralls type))
+           (synth-type (synth-hole type))
            )
       (when (string= name 'Hole)
         (insert-before-markers synth-type))
